0f57517ff9128dc30a236e8d2b6406511de7c78c
Aaron Den Hond <aaron.denhond@gmail.com>
2021-07-22 15:49:43 +0200

    working pagination instead of just page 1 now

--- first-react-api/src/Components/DataFetch.js
+++ first-react-api/src/Components/DataFetch.js
@@ -7,23 +7,35 @@ export default function DataFetch() {
   //useEffect hook for fetch
 
   useEffect(() => {
+    getData("https://swapi.dev/api/people");
+  }, []);
+
+  const getData = (url) => {
     axios
-      .get("https://swapi.dev/api/people")
+      .get(url)
       .then((res) => {
-        //get request returns a promise
-        console.log(res);
-        setPeople(res.data.results);
+        res.data.results.forEach((curResult) => {
+          setPeople((prevPeople) => {
+            return [...prevPeople, curResult];
+          });
+          console.log(curResult);
+        });
+
+        if (res.data.next) {
+          getData(res.data.next);
+        }
       })
       //catch errors if necessary
       .catch((err) => {
         console.log(err);
       });
-  }, []);
+  };
+
   return (
     <div>
       <ul>
         {people.map((person) => (
-          <li key={person.id}>{person.name}</li>
+          <li key={person.name}>{person.name}</li>
         ))}
       </ul>
     </div>
