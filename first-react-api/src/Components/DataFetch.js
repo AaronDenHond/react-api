12adcf74c8f3ccd0c90d8561161dae3bfa8cdac1
Aaron Den Hond <aaron.denhond@gmail.com>
2021-07-22 15:17:24 +0200

    working star wars api

--- first-react-api/src/Components/DataFetch.js
+++ first-react-api/src/Components/DataFetch.js
@@ -2,27 +2,28 @@ import { React, useState, useEffect } from "react";
 import axios from "axios";
 
 export default function DataFetch() {
-  const [posts, Setposts] = useState([]); //state variable,initialize posts to empty array.
+  const [people, setPeople] = useState([]); //state variable,initialize posts to empty array.
 
   //useEffect hook for fetch
 
   useEffect(() => {
     axios
-      .get("http://jsonplaceholder.typicode.com/posts")
+      .get("https://swapi.dev/api/people")
       .then((res) => {
         //get request returns a promise
         console.log(res);
+        setPeople(res.data.results);
       })
       //catch errors if necessary
       .catch((err) => {
         console.log(err);
       });
-  });
+  }, []);
   return (
     <div>
       <ul>
-        {posts.map((post) => (
-          <li></li>
+        {people.map((person) => (
+          <li key={person.id}>{person.name}</li>
         ))}
       </ul>
     </div>
